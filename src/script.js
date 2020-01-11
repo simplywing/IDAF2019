a3ada9d0ed98c5f20986d96f41e45d48d4bb6108
Joël Ammann <Joel.Ammann@leuchterag.ch>
2020-01-11 17:02:15 +0100

    Test mit echten Bildern

--- src/script.js
+++ src/script.js
@@ -1,3 +1,57 @@
+let ch, cw;
+let ctx;
+let img1, img2, img3, img4;
+
+function renderImageFullSize(imgObj, clearFirst){
+  if(clearFirst)
+  clearBackground();
+
+  let iwidth = imgObj.width / imgObj.height * ch;
+  let lborder = (cw - iwidth)/2;
+  ctx.drawImage(imgObj, lborder, 0, iwidth, ch);
+}
+
+function clearBackground(){
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, cw, ch);
+}
+
+function main(){
+  let mainCanvas = document.getElementById("mainCanvas");
+  mainCanvas.width  = window.innerWidth;
+  mainCanvas.height = window.innerHeight;
+
+  ctx = mainCanvas.getContext("2d");
+  ch = mainCanvas.height;
+  cw = mainCanvas.width;
+
+  img1 = document.getElementById("1_Vulkan");
+  img2 = document.getElementById("2_Tueren");
+  img3 = document.getElementById("3_Vulkanlandschaft");
+  img4 = document.getElementById("4_Vulkangeist");
+
+  renderImageFullSize(img1, true);
+
+  window.addEventListener("keydown", function(event){
+    if(event.code === "Digit1"){
+      renderImageFullSize(img1, true);
+    }
+    if (event.code === "Digit2") {
+      renderImageFullSize(img2, true);
+    }
+    if (event.code === "Digit3") {
+      renderImageFullSize(img3, true);      
+      ctx.drawImage(img4, 700, 50, 230, 400);
+    }
+    // if (event.code === "ArrowLeft") {
+    //     rotationX = rotationX + Math.PI * 0.01;
+    // }
+    // if (event.code === "ArrowRight") {
+    //     rotationX = rotationX - Math.PI * 0.01;
+    // }
+  })
+}
+
 // let req = new Request('monologe.json');
 
 // fetch(req)
@@ -12,28 +66,28 @@
 //     console.log(data);
 // });
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+// const delay = ms => new Promise(res => setTimeout(res, ms));
 
-let jsonobj;
-let r = new XMLHttpRequest();
-r.open("GET", "monologe.json", true);
-r.onreadystatechange = function () {
-  if (r.readyState != 4 || r.status != 200) return;
-  jsonobj = JSON.parse(r.responseText); main();
-};
-r.send();
+// let jsonobj;
+// let r = new XMLHttpRequest();
+// r.open("GET", "monologe.json", true);
+// r.onreadystatechange = function () {
+//   if (r.readyState != 4 || r.status != 200) return;
+//   jsonobj = JSON.parse(r.responseText); main();
+// };
+// r.send();
 
-const writeOutput = async (text, timeout) => {
-    await delay(timeout);
-    console.log(text);
-}
+// const writeOutput = async (text, timeout) => {
+//     await delay(timeout);
+//     console.log(text);
+// }
 
-function main(){
-    jsonobj.monologe[0].phrases.forEach(async (element) => {
-        writeOutput(element.phrase, element.duration);        
-    });
+// function main(){
+//     jsonobj.monologe[0].phrases.forEach(async (element) => {
+//         writeOutput(element.phrase, element.duration);        
+//     });
 
-}
+// }
 
 /*
 
